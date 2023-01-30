862948c8c772133eb38842bb509748e76f0513be
zakirhussainb <zakirhussainb2693@gmail.com>
2023-01-30 22:50:40 +0530

    Implemented LeftSide and ImageLayout Components

--- facebook-clone/src/component/mainpage/ImageLayout.js
+++ facebook-clone/src/component/mainpage/ImageLayout.js
@@ -11,10 +11,10 @@ class ImageLayout extends Component {
         return ( 
             <div className="imageLayout_container">
                 <div className="imageLayout_imglay">
-                    <Avatar className="imageLayout_img" src="https://scontent-pnq1-1.xx.fbcdn.net/v/t31.18172-1/16299632_1265392430180578_8956618037203765660_o.jpg?stp=dst-jpg_p148x148&_nc_cat=104&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=XyjfLOpDoyEAX9m-w0P&_nc_ht=scontent-pnq1-1.xx&oh=00_AfBIiha7dPNx7kAlbsLEZ7WP_6ricoYOi6g6huiLOT4NHA&oe=63FF53DF" />
+                    <Avatar className="imageLayout_img" src={this.props.image} />
                 </div>
                 <div className="imageLayout_text">
-                    Zakir
+                    {this.props.text}
                 </div>
             </div>
          );
--- facebook-clone/src/component/mainpage/leftsidepanel/LeftSide.js
+++ facebook-clone/src/component/mainpage/leftsidepanel/LeftSide.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import ImageLayout from '../ImageLayout';
 import "./LeftSide.css";
+import covid from "../../../images/covid.png";
+import groups from "../../../images/groups.png";
+import memories from "../../../images/memories.png";
 
 class LeftSide extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: []
+        }
     }
-    state = {  }
+    getData=() => {
+        let jsondata = [
+            {
+                "image" : covid,
+                "text" : "COVID-19 Information Centre"
+            },
+            {
+                "image" : groups,
+                "text" : "Friends"
+            },
+            {
+                "image" : memories,
+                "text" : "Memories"
+            }
+        ];
+        this.setState({data : jsondata});
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
     render() { 
         return ( 
             <div>
-                <ImageLayout />
-                <ImageLayout />
-                <ImageLayout />
-                <ImageLayout />
-                <ImageLayout />
-                <ImageLayout />
+                {
+                    this.state.data.map( (item) => (
+                        <ImageLayout image={item.image} text={item.text} />
+                    ))
+                }
             </div>
          );
     }
